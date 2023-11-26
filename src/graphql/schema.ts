e104a93c4673af31859b3f554993170f9fbddefd
Ben3940 <bty2015@comcast.net>
2023-11-26 13:12:12 -0500

    Added schema file for GraphQL API

---
+++ src/graphql/schema.ts
@@ -0,0 +1,14 @@
+export const typeDefs: string = `#graphql
+    type Food {
+        no: ID!
+        name: String!
+        serving_size: String!
+        calories: Int!
+        total_fat: String!
+        cholesterol: String!
+        sodium: String!
+        protein: String!
+        sugars: String!
+    }
+
+`;
