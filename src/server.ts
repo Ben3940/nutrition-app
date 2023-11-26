aa95ecdb9515e3b2e0a6140dbe5f5cf0fe69e3ce
Ben3940 <bty2015@comcast.net>
2023-11-26 13:07:25 -0500

    Created Apollo server file

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import { data } from './data/nutrition.tsx';
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from './graphql/schema.ts';
+
+const PORT: number = 4000;
+
+const resolvers = {
+  Query: {
+    food: () => data,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: PORT },
+});
+
+console.log(`Started Apollo server on port ${PORT} at ${url}`);
