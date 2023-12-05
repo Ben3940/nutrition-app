e8be5b897e3d2a610f70e9737de6e9eb777cbc78
Ben3940 <bty2015@comcast.net>
2023-12-04 20:36:32 -0500

    db_controller can initialize the database and insert values from nutrition.ts

--- graphql/src/db_controller.ts
+++ graphql/src/db_controller.ts
@@ -1,60 +1,46 @@
 import sqlite3 from 'sqlite3';
-
-const data: any = [
-  {
-    No: 0,
-    name: 'Cornstarch',
-    serving_size: '100 g',
-    calories: 381,
-    total_fat: '0.1g',
-    cholesterol: '0',
-    sodium: '9.00 mg',
-    protein: '0.26 g',
-    sugars: '0.00 g',
-  },
-  {
-    No: 1,
-    name: 'Nuts, pecans',
-    serving_size: '100 g',
-    calories: 691,
-    total_fat: '72g',
-    cholesterol: '0',
-    sodium: '0.00 mg',
-    protein: '9.17 g',
-    sugars: '3.97 g',
-  },
-  {
-    No: 2,
-    name: 'Eggplant, raw',
-    serving_size: '100 g',
-    calories: 25,
-    total_fat: '0.2g',
-    cholesterol: '0',
-    sodium: '2.00 mg',
-    protein: '0.98 g',
-    sugars: '3.53 g',
-  },
-];
+import { data } from './data/nutrition.js';
 
 const db = new sqlite3.Database(
-  '/graphql/dist/data/db.sqlite',
+  './dist/data/db.sqlite',
   sqlite3.OPEN_READWRITE
 );
 
-db.serialize(() => {
-  db.run('CREATE TABLE food (name TEXT, serving_size TEXT, calories INT)');
-
-  let sql = 'INSERT INTO food VALUES (?, ?, ?)';
-
-  data.forEach(({ name, serving_size, calories }) => {
-    db.run(sql, [name, serving_size, calories]);
-  });
-
-  db.each('SELECT * FROM food', (err, row) => {
-    console.log(
-      `Name: ${row.name}, Serv_Size: ${row.serving_size}, Cal: ${row.calories}`
+function init_table(table: string): void {
+  db.serialize(() => {
+    db.run(
+      `CREATE TABLE ${table} (name TEXT, serving_size TEXT, calories INT)`
     );
-  });
-});
 
+    let sql = 'INSERT INTO food VALUES (?, ?, ?)';
+
+    data.foods.forEach(({ name, serving_size, calories }) => {
+      db.run(sql, [name, serving_size, calories]);
+    });
+
+    db.each('SELECT * FROM food', (err, row) => {
+      console.log(
+        `Name: ${row.name}, Serv_Size: ${row.serving_size}, Cal: ${row.calories}`
+      );
+    });
+  });
+}
+
+function drop_table(table: string): void {
+  db.serialize(() => {
+    db.run(`DROP TABLE ${table}`);
+  });
+}
+
+function select_all_table(table: string): void {
+  db.serialize(() => {
+    db.each(`SELECT * FROM ${table}`, (err, row) => {
+      console.log(
+        `Name: ${row.name}, Serv_Size: ${row.serving_size}, Cal: ${row.calories}`
+      );
+    });
+  });
+}
+init_table('food');
+select_all_table('food');
 db.close();
