8146982943466e7ab6b07201f88bd627ba0faa6a
Ben3940 <bty2015@comcast.net>
2024-01-02 20:55:00 -0500

    Finished integrating 'better-sqlite3' and have started expanding the front-end to handle user input

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,27 +1,47 @@
 // import React from 'react';
 
 export function Header() {
+  const handle_form = (e) => {
+    e.preventDefault();
+    console.log(e);
+    console.dir(e);
+  };
   return (
     <header>
       <h1>Nu-TRIS</h1>
-      <form action='' className='form-container'>
-        <label htmlFor='name'>Name:</label>
-        <input type='text' name='name' id='name' />
+      <form className='form-container' onSubmit={handle_form}>
+        <div className='input-container'>
+          <label htmlFor='name'>Name:</label>
+          <input type='text' name='name' id='name' />
+        </div>
 
-        <label htmlFor='calories'>Calories:</label>
-        <input type='number' name='calories' id='calories' />
+        <div className='input-container'>
+          <label htmlFor='calories'>Calories:</label>
+          <input type='number' name='calories' id='calories' />
+        </div>
 
-        <label htmlFor='fat'>Fat:</label>
-        <input type='number' name='fat' id='fat' />
+        <div className='input-container'>
+          <label htmlFor='fat'>Fat:</label>
+          <input type='number' name='fat' id='fat' />
+        </div>
 
-        <label htmlFor='protien'>Protien:</label>
-        <input type='number' name='protien' id='protien' />
+        <div className='input-container'>
+          <label htmlFor='protien'>Protien:</label>
+          <input type='number' name='protien' id='protien' />
+        </div>
 
-        <label htmlFor='sodium'>Sodium:</label>
-        <input type='number' name='sodium' id='sodium' />
+        <div className='input-container'>
+          <label htmlFor='sodium'>Sodium:</label>
+          <input type='number' name='sodium' id='sodium' />
+        </div>
 
-        <label htmlFor='sugar'>Sugar:</label>
-        <input type='number' name='sugar' id='sugar' />
+        <div className='input-container'>
+          <label htmlFor='sugar'>Sugar:</label>
+          <input type='number' name='sugar' id='sugar' />
+        </div>
+        <button className='submit-button' type='submit'>
+          Search
+        </button>
       </form>
     </header>
   );
