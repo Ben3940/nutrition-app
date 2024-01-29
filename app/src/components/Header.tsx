7e49dbbb78b7e865a45b1012f2fa7a32648da8bb
Ben3940 <bty2015@comcast.net>
2024-01-29 16:24:01 -0500

    Added html for filters to retrieve data based on less/greater values of specific nutrition value

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -40,14 +40,134 @@ export function Header({ names, handle_form, get_all }: header) {
         <div className='input-container'></div>
         <div className='input-container'>
           <div className='radio'>
-            <input type='radio' name='ordering' id='radio-desc' value='desc' />
-            <label htmlFor='radio-desc'>Descending</label>
+            <input type='radio' name='ordering' id='radio-less' value='less' />
+            <label htmlFor='radio-less'>Less Than</label>
           </div>
           <div className='radio'>
-            <input type='radio' name='ordering' id='radio-asc' value='asc' />
-            <label htmlFor='radio-asc'>Ascending</label>
+            <input
+              type='radio'
+              name='ordering'
+              id='radio-greater'
+              value='greater'
+            />
+            <label htmlFor='radio-greater'>Greater Than</label>
           </div>
         </div>
+        <div className='input-container'>
+          <label htmlFor='calories'>Calories:</label>
+          <input
+            type='number'
+            name='calories'
+            id='calories'
+            min='0'
+            max='1000'
+          />
+          <div className='radio'>
+            <input type='radio' name='ordering' id='radio-less' value='less' />
+            <label htmlFor='radio-less'>&le;</label>
+          </div>
+          <div className='radio'>
+            <input
+              type='radio'
+              name='ordering'
+              id='radio-greater'
+              value='greater'
+            />
+            <label htmlFor='radio-greater'>&ge;</label>
+          </div>
+        </div>
+        <div className='input-container'>
+          <label htmlFor='sodium'>Sodium:</label>
+          <input type='number' name='sodium' id='sodium' min='0' max='100' />
+          <div className='radio'>
+            <input type='radio' name='ordering' id='radio-less' value='less' />
+            <label htmlFor='radio-less'>&le;</label>
+          </div>
+          <div className='radio'>
+            <input
+              type='radio'
+              name='ordering'
+              id='radio-greater'
+              value='greater'
+            />
+            <label htmlFor='radio-greater'>&ge;</label>
+          </div>
+        </div>
+        <div className='input-container'>
+          <label htmlFor='fat'>Fat:</label>
+          <input type='number' name='fat' id='fat' min='0' max='100' />
+          <div className='radio'>
+            <input type='radio' name='ordering' id='radio-less' value='less' />
+            <label htmlFor='radio-less'>&le;</label>
+          </div>
+          <div className='radio'>
+            <input
+              type='radio'
+              name='ordering'
+              id='radio-greater'
+              value='greater'
+            />
+            <label htmlFor='radio-greater'>&ge;</label>
+          </div>
+        </div>
+        <div className='input-container'>
+          <label htmlFor='sugar'>Sugar:</label>
+          <input type='number' name='sugar' id='sugar' min='0' max='100' />
+          <div className='radio'>
+            <input type='radio' name='ordering' id='radio-less' value='less' />
+            <label htmlFor='radio-less'>&le;</label>
+          </div>
+          <div className='radio'>
+            <input
+              type='radio'
+              name='ordering'
+              id='radio-greater'
+              value='greater'
+            />
+            <label htmlFor='radio-greater'>&ge;</label>
+          </div>
+        </div>
+        <div className='input-container'>
+          <label htmlFor='cholesterol'>Cholest:</label>
+          <input
+            type='number'
+            name='cholesterol'
+            id='cholesterol'
+            min='0'
+            max='100'
+          />
+          <div className='radio'>
+            <input type='radio' name='ordering' id='radio-less' value='less' />
+            <label htmlFor='radio-less'>&le;</label>
+          </div>
+          <div className='radio'>
+            <input
+              type='radio'
+              name='ordering'
+              id='radio-greater'
+              value='greater'
+            />
+            <label htmlFor='radio-greater'>&ge;</label>
+          </div>
+        </div>
+        <div className='input-container'>
+          <label htmlFor='protein'>Protein:</label>
+          <input type='number' name='protein' id='protein' min='0' max='100' />
+          <div className='radio'>
+            <input type='radio' name='ordering' id='radio-less' value='less' />
+            <label htmlFor='radio-less'>&le;</label>
+          </div>
+          <div className='radio'>
+            <input
+              type='radio'
+              name='ordering'
+              id='radio-greater'
+              value='greater'
+            />
+            <label htmlFor='radio-greater'>&ge;</label>
+          </div>
+        </div>
+
         <button className='submit-button' type='submit'>
           Search
         </button>
