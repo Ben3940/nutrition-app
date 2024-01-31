f63d486ae44075ee42ff661bbee29e5b138c4023
Ben3940 <bty2015@comcast.net>
2024-01-31 16:31:34 -0500

    Implemented skeleton structure of Nutrition_Field component.  Need to work on props for component

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { form_function } from '../types/form_function';
+import { Nutrition_Field } from './Nutrition_Field';
 
 // import React from 'react';
 {
@@ -15,6 +16,14 @@ interface header {
 }
 
 export function Header({ names, handle_form, get_all }: header) {
+  const nutrition_names: string[] = [
+    'Calories',
+    'Sodium',
+    'Fat',
+    'Cholest',
+    'Protein',
+  ];
+
   return (
     <header>
       <h1>Nu-TRIS</h1>
@@ -24,134 +33,21 @@ export function Header({ names, handle_form, get_all }: header) {
           <input type='number' name='no' id='no' min='0' max='24' />
         </div>
 
-        <div className='input-container'>
-          <label htmlFor='name'>Name:</label>
-          <select name='name' id='name'>
-            <option value={''}></option>
-            {names.map((name: string) => {
-              return (
-                <option key={name} value={name}>
-                  {name}
-                </option>
-              );
-            })}
-          </select>
-        </div>
-
-        <div className='input-container'>
-          <label htmlFor='calories'>Calories:</label>
-          <input
-            type='number'
-            name='calories'
-            id='calories'
-            min='0'
-            max='1000'
-          />
-          <div className='radio'>
-            <input type='radio' name='ordering' id='radio-less' value='less' />
-            <label htmlFor='radio-less'>&le;</label>
-          </div>
-          <div className='radio'>
-            <input
-              type='radio'
-              name='ordering'
-              id='radio-greater'
-              value='greater'
-            />
-            <label htmlFor='radio-greater'>&ge;</label>
-          </div>
-        </div>
-        <div className='input-container'>
-          <label htmlFor='sodium'>Sodium:</label>
-          <input type='number' name='sodium' id='sodium' min='0' max='100' />
-          <div className='radio'>
-            <input type='radio' name='ordering' id='radio-less' value='less' />
-            <label htmlFor='radio-less'>&le;</label>
-          </div>
-          <div className='radio'>
-            <input
-              type='radio'
-              name='ordering'
-              id='radio-greater'
-              value='greater'
-            />
-            <label htmlFor='radio-greater'>&ge;</label>
-          </div>
-        </div>
-        <div className='input-container'>
-          <label htmlFor='fat'>Fat:</label>
-          <input type='number' name='fat' id='fat' min='0' max='100' />
-          <div className='radio'>
-            <input type='radio' name='ordering' id='radio-less' value='less' />
-            <label htmlFor='radio-less'>&le;</label>
-          </div>
-          <div className='radio'>
-            <input
-              type='radio'
-              name='ordering'
-              id='radio-greater'
-              value='greater'
-            />
-            <label htmlFor='radio-greater'>&ge;</label>
-          </div>
-        </div>
-        <div className='input-container'>
-          <label htmlFor='sugar'>Sugar:</label>
-          <input type='number' name='sugar' id='sugar' min='0' max='100' />
-          <div className='radio'>
-            <input type='radio' name='ordering' id='radio-less' value='less' />
-            <label htmlFor='radio-less'>&le;</label>
-          </div>
-          <div className='radio'>
-            <input
-              type='radio'
-              name='ordering'
-              id='radio-greater'
-              value='greater'
-            />
-            <label htmlFor='radio-greater'>&ge;</label>
-          </div>
-        </div>
-        <div className='input-container'>
-          <label htmlFor='cholesterol'>Cholest:</label>
-          <input
-            type='number'
-            name='cholesterol'
-            id='cholesterol'
-            min='0'
-            max='100'
-          />
-          <div className='radio'>
-            <input type='radio' name='ordering' id='radio-less' value='less' />
-            <label htmlFor='radio-less'>&le;</label>
-          </div>
-          <div className='radio'>
-            <input
-              type='radio'
-              name='ordering'
-              id='radio-greater'
-              value='greater'
-            />
-            <label htmlFor='radio-greater'>&ge;</label>
-          </div>
-        </div>
-        <div className='input-container'>
-          <label htmlFor='protein'>Protein:</label>
-          <input type='number' name='protein' id='protein' min='0' max='100' />
-          <div className='radio'>
-            <input type='radio' name='ordering' id='radio-less' value='less' />
-            <label htmlFor='radio-less'>&le;</label>
-          </div>
-          <div className='radio'>
-            <input
-              type='radio'
-              name='ordering'
-              id='radio-greater'
-              value='greater'
-            />
-            <label htmlFor='radio-greater'>&ge;</label>
-          </div>
-        </div>
+        {nutrition_names.map((field_name) => {
+          if (field_name === 'Calroies') {
+            return (
+              <Nutrition_Field
+                key={field_name}
+                name={field_name}
+                max_val={1000}
+                unit={'cals'}
+              />
+            );
+          }
+          return (
+            <Nutrition_Field key={field_name} name={field_name} unit={'mg'} />
+          );
+        })}
 
         <button className='submit-button' type='submit'>
           Search
--- app/src/components/Nutrition_Field.tsx
+++ app/src/components/Nutrition_Field.tsx
@@ -1,14 +1,27 @@
-export function Nutrition_Field() {
-  <div className='input-container'>
-    <label htmlFor='calories'>Calories:</label>
-    <input type='number' name='calories' id='calories' min='0' max='1000' />
-    <div className='radio'>
-      <input type='radio' name='ordering' id='radio-less' value='less' />
-      <label htmlFor='radio-less'>&le;</label>
+interface field {
+  name: string;
+  max_val?: number;
+  unit: string;
+}
+
+export function Nutrition_Field({ name, max_val = 1000, unit }: field) {
+  return (
+    <div className='input-container'>
+      <label htmlFor='{name}'>{name}:</label>
+      <input type='number' name={name} id={name} min='0' max={max_val} />
+      <div className='radio'>
+        <input type='radio' name='ordering' id='radio-less' value='less' />
+        <label htmlFor='radio-less'>&le;</label>
+      </div>
+      <div className='radio'>
+        <input
+          type='radio'
+          name='ordering'
+          id='radio-greater'
+          value='greater'
+        />
+        <label htmlFor='radio-greater'>&ge;</label>
+      </div>
     </div>
-    <div className='radio'>
-      <input type='radio' name='ordering' id='radio-greater' value='greater' />
-      <label htmlFor='radio-greater'>&ge;</label>
-    </div>
-  </div>;
+  );
 }
